0f19622a061aaeec9b537aebfbfd2d43fc81f598
Sarah Allen <sarah@ultrasaurus.com>
2013-09-03 21:03:37 -0700

    add a traditional tab style

--- public/style123.js
+++ public/style123.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+
+function style123_init() {
   console.log('style123');
   $(".style123 li a .section-title").each(function(index, elem) {
     console.log(index);
@@ -15,5 +16,14 @@ $(document).ready(function(){
       $(evt.currentTarget).removeClass('hover');
     }
   );
+}
+
+function style123_remove() {
+  $(".style123 .filled-circle").remove();
+  $(".style123 .step-num").remove();
+}
+
+$(document).ready(function() {
+  style123_init();
 });
 
